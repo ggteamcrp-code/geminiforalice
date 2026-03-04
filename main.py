4f70898c1748d9f4e7426ddeea72e0ee98aee173
ggteamcrp-code <ggteamcrp@gmail.com>
2026-03-04 19:33:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,6 @@ def main():
 
 if __name__ == '__main__':
     # Railway сам скажет приложению, на каком порту работать
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
